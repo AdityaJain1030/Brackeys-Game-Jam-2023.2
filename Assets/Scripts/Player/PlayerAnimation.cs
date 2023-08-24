081d13ab094a8f3530724824f5087c46a887ee28
Arhum Jain <arhumjain10@gmail.com>
2023-08-24 01:09:21 -0700

    fix attack toonlit

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -31,9 +31,19 @@ public class PlayerAnimation : BaseAnimation
         animator.SetBool("slashingOutwards", playerCombatScript.IsOutwardSlashing);
         animator.SetBool("slashingAcross", playerCombatScript.IsAcrossSlashing);
 
+        Debug.Log(playerCombatScript.IsInwardSlashing + " ");
         if (playerMovementScript.IsRunning) {
             spriteRenderer.material = materials[1];
-        } else {
+        } else if (playerCombatScript.IsInwardSlashing) {
+            spriteRenderer.material = materials[2];
+            Debug.Log(materials[2].name);
+        } else if (playerCombatScript.IsOutwardSlashing) {
+            spriteRenderer.material = materials[3];
+            Debug.Log(materials[3].name);
+        } else if (playerCombatScript.IsAcrossSlashing) {
+            spriteRenderer.material = materials[4];
+            Debug.Log(materials[4].name);
+        } else if (animator.GetCurrentAnimatorStateInfo(0).IsName("AdventurerIdle")) {
             spriteRenderer.material = materials[0];
         }
         
@@ -43,13 +53,15 @@ public class PlayerAnimation : BaseAnimation
             spriteRenderer.flipX = true;
         }
 
-        if (playerCombatScript.IsInwardSlashing) {
-            // Debug.Log("INWARDS");
-            spriteRenderer.material = materials[2];
-        } else if (playerCombatScript.IsOutwardSlashing) {
-            spriteRenderer.material = materials[3];
-        } else if (playerCombatScript.IsAcrossSlashing) {
-            spriteRenderer.material = materials[4];
-        }
+        // if (playerCombatScript.IsInwardSlashing) {
+        //     // Debug.Log("INWARDS");
+        //     spriteRenderer.material = materials[2];
+        // }
+        // if (playerCombatScript.IsOutwardSlashing) {
+        //     spriteRenderer.material = materials[3];
+        // }
+        // if (playerCombatScript.IsAcrossSlashing) {
+        //     spriteRenderer.material = materials[4];
+        // }
     }
 }
